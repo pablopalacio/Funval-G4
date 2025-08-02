b0af3d52a5f9b3e46a80461a6e95522017bf029a
Pablop <palaciopablo92@gmail.com>
2025-08-02 03:59:24 -0300

    funval

--- individual/main.js
+++ individual/main.js
@@ -1,5 +1,6 @@
-    const menuBtn = document.getElementById('menu-btn');
-    const menu = document.getElementById('menu');
-    menuBtn.addEventListener('click', () => {
-      menu.classList.toggle('hidden');
-    });
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+});
