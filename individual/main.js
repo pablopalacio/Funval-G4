04af0d7af532be6ac5b6e01d4856782983980c03
Pablop <palaciopablo92@gmail.com>
2025-08-04 12:36:16 -0300

    mod

--- individual/main.js
+++ individual/main.js
@@ -1,6 +1,25 @@
 const menuBtn = document.getElementById('menu-btn');
+const cerrarBtn = document.getElementById('cerrar-menu');
 const menu = document.getElementById('menu');
+const links = document.querySelectorAll('.menu-link');
+const secciones = ['home-content', 'servicios-content', 'academia-content', 'links-content'];
 
 menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
+  menu.classList.remove('hidden');
+});
+
+cerrarBtn.addEventListener('click', () => {
+  menu.classList.add('hidden');
+});
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = link.getAttribute('data-target');
+    secciones.forEach(id => {
+      document.getElementById(id).classList.add('hidden');
+    });
+    document.getElementById(target).classList.remove('hidden');
+    menu.classList.add('hidden');
+  });
 });
